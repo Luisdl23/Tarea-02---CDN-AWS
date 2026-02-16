579385870f88961407d2b09dce0c1793d2cbee00
Luisdl <quique23@umes.edu.gt>
2026-02-15 18:17:19 -0600

    Se crea todo, la pagina del clima

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="title">CLIMA</h1>
+      <div className="card main-card">
+        <h2>Guatemala</h2>
+        <p className="temp">23°C</p>
+        <p>Humedad: 50%</p>
+        <p>Viento: 6 km/h</p>
+      </div>
+
+      <div className="forecast">
+        <div className="card">
+          <h3>Lun</h3>
+          <p>26°C</p>
+        </div>
+        <div className="card">
+          <h3>Mar</h3>
+          <p>27°C</p>
+        </div>
+        <div className="card">
+          <h3>Mié</h3>
+          <p>27°C</p>
+        </div>
+        <div className="card">
+          <h3>Jue</h3>
+          <p>27°C</p>
+        </div>
+        <div className="card">
+          <h3>Vie</h3>
+          <p>27°C</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
